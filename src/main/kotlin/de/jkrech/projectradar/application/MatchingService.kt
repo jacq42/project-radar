33899dfea08cacc7e4f23f9bbb70899894c8d4f3
jacq42 <jacq42@users.noreply.github.com>
2025-07-23 11:59:31 +0200

    refactor matching service

--- src/main/kotlin/de/jkrech/projectradar/application/MatchingService.kt
+++ src/main/kotlin/de/jkrech/projectradar/application/MatchingService.kt
@@ -1,6 +1,7 @@
 package de.jkrech.projectradar.application
 
 import org.slf4j.LoggerFactory
+import org.springframework.ai.document.Document
 import org.springframework.ai.embedding.EmbeddingModel
 import org.springframework.ai.embedding.TokenCountBatchingStrategy
 import org.springframework.ai.openai.OpenAiEmbeddingOptions
@@ -14,28 +15,30 @@ class MatchingService(
 ) {
     private val logger = LoggerFactory.getLogger(MatchingService::class.java)
 
+    private val embeddingOptions = OpenAiEmbeddingOptions.builder().build()
+    private val batchingStrategy= TokenCountBatchingStrategy()
+
     fun findMatches() {
         val profileData = profileReader.read()
         logger.info("Found ${profileData.size} documents in profile")
 
-//        val embeddingProfile = embeddingModel.embed(
-//            profileData,
-//            OpenAiEmbeddingOptions.builder().build(),
-//            TokenCountBatchingStrategy()
-//        )
-//        logger.info("Embedding response: $embeddingProfile")
+        val embeddingProfile = embedDocuments(profileData)
+        logger.info("Embedding response: $embeddingProfile")
 
         projectsImporters.forEach { importer ->
             logger.info("Importing projects with ${importer::class.simpleName}")
             val projectData = importer.import()
             logger.info("Found ${projectData.size} documents in project")
-//            val embeddingProject = embeddingModel.embed(
-//                projectData,
-//                OpenAiEmbeddingOptions.builder().build(),
-//                TokenCountBatchingStrategy()
-//            )
-//            logger.info("Embedding response: $embeddingProject")
+            val embeddingProject = embedDocuments(projectData)
+            logger.info("Embedding response: $embeddingProject")
         }
+    }
 
+    private fun embedDocuments(documents: List<Document>): MutableList<FloatArray> {
+        return embeddingModel.embed(
+            documents,
+            embeddingOptions,
+            batchingStrategy
+        )
     }
 }
